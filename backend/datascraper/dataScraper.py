fa57b675a465c5420cccac106f3dea5603f0a45a
C.J <csperber@yahoo.com>
2014-01-25 17:28:40 -0800

    dataScraper.py updated with mechanize.py; will now grab uris from google

--- backend/datascraper/dataScraper.py
+++ backend/datascraper/dataScraper.py
@@ -1,28 +1,76 @@
+# startup weekend euro, 1/25/2014, seattle, wa
+# life path
+# designed by cj sperber
+
 import sys
 import re
-from urllib2 import Request, urlopen
-import urllib
+from time import sleep
+from random import randint
+import mechanize
+import cookielib
 from bs4 import BeautifulSoup
 
-def grabLinkedInData(uri = '', queryString = ''):
-    constructedUri = uri % urllib.quote_plus(queryString)
+# Browser
+br = mechanize.Browser()
 
-    req_google = Request(constructedUri)
-    req_google.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB;    rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
 
-    html_google = urlopen(req_google).read()
+# Browser options
+br.set_handle_equiv(True)
+br.set_handle_gzip(False)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
 
-    soup = BeautifulSoup(html_google)
+# Follows refresh 0 but not hangs on refresh > 0
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-    regex = ur"http://www.linkedin.com/in/[^&%]*"
+#br.set_debug_http(True)
+#br.set_debug_redirects(True)
+#br.set_debug_responses(True)
+
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+
+def getLinkedInUris(startRec=0):
+    r = br.open('https://www.google.com/search?q=site%3Awww.linkedin.com%2Fin&start=' + str(startRec))
+    html = r.read()
+
+    # Show the source:
+    #print html or br.response().read()
+
+    soup = BeautifulSoup(html)
+
+    regex = ur"http://www.linkedin.com/in/[^&% ]*"
+
+    allLinksFound = []
 
     for link in soup.find_all('a'):
         theLink = link.get('href')
+        #print theLink
         if len(theLink) > 1:
             #print "Link = " + theLink
             linksFound = re.findall(regex, theLink)
             if len(linksFound) > 0:
                 #print "Link = " + theLink
-                print linksFound
+                if linksFound[0] not in allLinksFound:
+                    allLinksFound.append(linksFound[0])
 
-grabLinkedInData('http://www.google.com/search?q=%s', 'site:www.linkedin.com/in')
+    return allLinksFound
+
+
+def getLinkedInUrisByLoopsOfTen(loopsOfTen=10, debug=False):
+    for i in xrange(0, loopsOfTen):
+        uris = getLinkedInUris(i*10)
+        for uri in uris:
+            print uri
+
+        # delay
+        sleepSecs = randint(1,4)
+        if debug == True:
+            sys.stderr.write("Loop %d, Delaying ...%s\n" % (i, str(sleepSecs)))
+        sleep(sleepSecs)
+
+getLinkedInUrisByLoopsOfTen(100, True)
